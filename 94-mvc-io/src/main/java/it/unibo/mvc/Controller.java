c4808086e79b415be68e2c3f798cec759e0955f3
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2023-11-21 11:59:32 +0100

    errore di comprensione del testo, ho risoluto durante la implementazione di SimpleController

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,7 +1,5 @@
 package it.unibo.mvc;
 
-import java.util.List;
-
 /**
  * it must model a simple controller responsible of I/O access.
  * It considers only the standard output, and it is able to print on it.
@@ -9,28 +7,9 @@ import java.util.List;
 public interface Controller {
 
     /**
-     * A method for setting the next string to print. Null values are not
-     * acceptable, and an exception should be produced
-     * @param data is the message to be set
+     * Prints on the standard output the current message
      */
-    public void setNextMessage (String data);
+    public void printOnStdout ();
 
-    /**
-     * A method for getting the next string to print
-     * @return the message
-     */
-    public String getNextMessage ();
-
-    /**
-     * A method for getting the history of the printed strings (in form of a `List` of `Strings`)
-     * @return the List of the printed Strings starting from the oldest to the newest one
-     */
-    public List<String> getHistory ();
-    
-    /**
-     * A method that prints the current string.
-     * If the current string is unset, an `IllegalStateException` should be thrown
-     */
-    public void printMessage ();
 
 }
--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,60 @@
 package it.unibo.mvc;
 
+import java.io.PrintStream;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private final PrintStream LOG = System.out;
+    private List<String> history = new ArrayList<>();
+    private String currentMsg;
+
+    @Override
+    public void printOnStdout() {
+        LOG.println(currentMsg);
+    }
+
+    /**
+     * A method for setting the next string to print. Null values are not
+     * acceptable, and an exception should be produced
+     * @param data is the message to be set
+     */
+    public void setNextMessage (final String data) {
+        this.currentMsg = data;
+        this.history.add(this.currentMsg);
+    }
+
+    /**
+     * A method for getting the next string to print
+     * @return the message
+     */
+    public String getNextMessage () {
+        return this.currentMsg;
+    }
+
+    /**
+     * A method for getting the history of the printed strings (in form of a `List` of `Strings`)
+     * @return the List of the printed Strings starting from the oldest to the newest one
+     */
+    public List<String> getHistory () {
+        return new ArrayList<String>(this.history);
+    }
+    
+    /**
+     * A method that prints the current string.
+     * If the current string is unset, an `IllegalStateException` should be thrown
+     */
+    public void printMessage () throws IllegalStateException {
+        if (this.currentMsg == null) {
+            throw new IllegalStateException("The current message is unset");
+        }
+        printOnStdout();
+    }
 
 }
